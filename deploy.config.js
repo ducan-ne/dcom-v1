392e6b7181278f09a45fbd71d2d14d9e60bfb8fc
ancmsvn <me@clgt.io>
2019-04-16 03:04:21 +0700

    refactor

--- deploy.config.js
+++ deploy.config.js
@@ -18,7 +18,7 @@ module.exports = {
       host: ['159.65.5.25'], // ip
       ref: 'origin/master',
       repo: 'git@github.com:quaj/my-error-logeer.git',
-      path: '/root/error-logger',
+      path: '/root/dcom-v1',
       'post-deploy': 'yarn && pm2 reload deploy.config.js --env production',
       ssh_options: ['StrictHostKeyChecking=no']
     }
